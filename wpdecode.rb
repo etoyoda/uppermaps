47457904068d4ffed3fccd627d8e1ca835f9ab34
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-23 03:00:02 +0000

    toriaezu-decode

--- wpdecode.rb
+++ wpdecode.rb
@@ -33,6 +33,10 @@ class WPDecode
 
   def subset_flush
     return unless @sdb
+    for iter in @sdb.keys.grep(/^R=/)
+      rt = Time.gm(*@sdb[iter].values_at(*%w(rty rtm rtd rth rtn)))
+      @sdb[iter]['rt'] = rt
+    end
     stnid = @sdb.values_at('001001', '001002').join
     @db[stnid] = @sdb
     @sdb = nil
@@ -44,9 +48,39 @@ class WPDecode
   end
 
   def subset_data desc, val
+    return if val == 'MSNG'
     case desc
-    when /^(00100[12]|00[56]002|007001)$/ then
-      @sdb[desc] = val
+    when /^00100[12]$/ then @sdb[desc] = val
+    when '005002' then @sdb['lat'] = val.to_f
+    when '006002' then @sdb['lon'] = val.to_f
+    when '007001' then @sdb['hmsl'] = val.to_i
+    end
+  end
+
+  def pulse_data iter, desc, val
+    return if val == 'MSNG'
+    @sdb[iter] = { 'rt' => nil } unless @sdb[iter]
+    case desc
+    when '004001' then @sdb[iter]['rty'] = val.to_i
+    when '004002' then @sdb[iter]['rtm'] = val.to_i
+    when '004003' then @sdb[iter]['rtd'] = val.to_i
+    when '004004' then @sdb[iter]['rth'] = val.to_i
+    when '004005' then @sdb[iter]['rtn'] = val.to_i
+    end
+  end
+
+  def point_data iter, iter2, desc, val
+    return if val == 'MSNG'
+    unless @sdb[iter] then
+      $stderr.puts "undated pulse skipped" if $DEBUG
+    end
+    @sdb[iter][iter2] = {} unless @sdb[iter][iter2]
+    case desc
+    when '011003' then @sdb[iter][iter2]['u'] = val.to_f
+    when '011004' then @sdb[iter][iter2]['v'] = val.to_f
+    when '011006' then @sdb[iter][iter2]['w'] = val.to_f
+    when '021030' then @sdb[iter][iter2]['signal'] = val.to_f
+    when '007006' then @sdb[iter][iter2]['gph'] = val.to_i
     end
   end
 
@@ -55,8 +89,11 @@ class WPDecode
       case line.chomp
       when /^DATASUBSET \d+ :/ then
         subset_ini
-      when /^(0\d{5}) {R=(\d+)\.(\d+)}(?:{\S*})? / then
-      when /^(0\d{5}) {R=(\d+)} / then
+      when /^031/ then :do_nothing
+      when /^(0\d{5}) {((R=\d+)\.\d+)}(?:{\S*})? / then
+        point_data($3, $2, $1, $')
+      when /^(0\d{5}) {(R=\d+)} / then
+        pulse_data($2, $1, $')
       when /^(0\d{5}) / then
         subset_data($1, $')
       end
