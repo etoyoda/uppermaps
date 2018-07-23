62c619d97ddf1b5f2971cb0438f8acc8e95bac0f
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-23 03:00:01 +0000

    toriaezu

--- wpdecode.rb
+++ wpdecode.rb
@@ -28,16 +28,56 @@ class WPDecode
 
   def initialize
     @db = {}
+    @sdb = nil
   end
 
-  def run args
-    args.each {|arg|
-      File.open(arg) {|fp|
-        
-      }
+  def subset_flush
+    return unless @sdb
+    stnid = @sdb.values_at('001001', '001002').join
+    @db[stnid] = @sdb
+    @sdb = nil
+  end
+
+  def subset_ini
+    subset_flush
+    @sdb = { "001001" => "//", "001002" => "///" }
+  end
+
+  def subset_data desc, val
+    case desc
+    when /^(00100[12]|00[56]002|007001)$/ then
+      @sdb[desc] = val
+    end
+  end
+
+  def scanfp fp
+    fp.each_line {|line|
+      case line.chomp
+      when /^DATASUBSET \d+ :/ then
+        subset_ini
+      when /^(0\d{5}) {R=(\d+)\.(\d+)}(?:{\S*})? / then
+      when /^(0\d{5}) {R=(\d+)} / then
+      when /^(0\d{5}) / then
+        subset_data($1, $')
+      end
     }
+    subset_flush
+  end
+
+  def scanfile filename
+    File.open(filename) {|fp| scanfp(fp) }
+    self
+  end
+
+  def scanfiles args
+    args.each {|arg| scanfile(arg) }
+    self
+  end
+
+  def dump
+    $stdout.write(JSON.pretty_generate(@db))
   end
 
 end
 
-WPDecode.new.run(ARGV)
+WPDecode.new.scanfiles(ARGV).dump
