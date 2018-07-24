a61a1baa8dd4f877c823986a5ef1e9bdc72d0bb6
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-24 00:00:01 +0000

    toriaezu

--- hourly.sh
+++ hourly.sh
@@ -2,15 +2,26 @@
 
 cd `/usr/bin/dirname $0`
 
+set -e
+
 cutoff=40
 timecard=`/usr/bin/ruby -e "puts((Time.now - $cutoff * 60).strftime('%Y %m %d %H 00'))"`
 
-test -d tmp || mkdir tmp
-(cd tmp ; sh -$- local-collect.sh $timecard 'A_IUPC[45][0-9]RJTD')
+if test -d tmp
+then
+  test ! -d tmp.prev || rm -rf tmp.prev
+  mv tmp tmp.prev
+  echo found work directory by previous run for $(cat tmp/timecard.txt).
+fi
+mkdir tmp
+echo $timecard > tmp/timecard.txt
 
+(cd tmp ; sh -$- local-collect.sh $timecard 'A_IUPC[45][0-9]RJTD')
 for bufr in tmp/A*.bufr
 do
 bufr_decoder -dump -output tmp/z.txt -inbufr $bufr
-cat tmp/z.txt >> tmp/iupcrjtd.txt 
+cat tmp/z.txt >> tmp/wpjp.txt 
 done
-ruby wpdecode.rb tmp/iupcrjtd.txt > wpjp.json
+ruby wpdecode.rb tmp/wpjp.txt > wpjp.json
+
+test -f tmp.keep || rm -rf tmp
